9ff9ba756e529b9ccca22fe7f02b6efeb927b001
Zayed Bin Gazi <zayedbingazi@Zayeds-MacBook-Air.local>
2023-12-13 14:59:58 +0600

    github data added

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 3000
+
+const gitHubData = {
+    "login": "zayedbingazidev",
+    "id": 54660577,
+    "node_id": "MDQ6VXNlcjU0NjYwNTc3",
+    "avatar_url": "https://avatars.githubusercontent.com/u/54660577?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/zayedbingazidev",
+    "html_url": "https://github.com/zayedbingazidev",
+    "followers_url": "https://api.github.com/users/zayedbingazidev/followers",
+    "following_url": "https://api.github.com/users/zayedbingazidev/following{/other_user}",
+    "gists_url": "https://api.github.com/users/zayedbingazidev/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/zayedbingazidev/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/zayedbingazidev/subscriptions",
+    "organizations_url": "https://api.github.com/users/zayedbingazidev/orgs",
+    "repos_url": "https://api.github.com/users/zayedbingazidev/repos",
+    "events_url": "https://api.github.com/users/zayedbingazidev/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/zayedbingazidev/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Zayed",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 12,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2019-08-29T06:52:58Z",
+    "updated_at": "2023-12-13T08:38:27Z"
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -17,6 +52,10 @@ app.get('/login', (req, res) => {
 
 app.get('/youtube', (req, res) => {
     res.send('<h2>Money</h2>')
+})
+
+app.get('/github', (req, res) => {
+    res.json(gitHubData)
 })
 
 app.listen(process.env.PORT, () => {
